70138121677a4355efebdcbee5ca956c82fda1dc
yuhui-zh15 <yuhui-zh15@mails.tsinghua.edu.cn>
2018-05-18 09:50:59 +0800

    modify main

--- main.py
+++ main.py
@@ -1,23 +1,19 @@
 from flask import Flask, Response
-from render.render import *
-from db.db import *
+from synth.synth import synth, generate
 import time
 app = Flask(__name__)
 
          
 @app.route('/image/<userid>')
 def index(userid):
-    userjson = get_data2(str(userid))
-    if userjson is None: return 'User Not Exist'
+    result = synth(generate(userid))
     filename = '/home/yuhui/xuetangxapi/buffer/' + str(time.time()) + '.png'
-    render2(userjson, filename)
+    result.save(filename)
     image = file(filename)
     resp = Response(image, mimetype='image/jpeg')
     return resp
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=8080)
-    userjson = get_data2(str('473725'))
-    render2(userjson, 'out.png')
+    app.run(host='0.0.0.0', port=8080)
 
     
